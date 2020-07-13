b30c051d59756add7bb332d5284ff54c3b71ec03
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-07-13 09:05:19 -0700

    implemented LindedList class

--- linked_list.py
+++ linked_list.py
@@ -16,5 +16,30 @@ class Node:
         self.next = new_next
 
 
+class LinkedList:
+    def __init__(self)"
+    # the first Node in the LinkedList 
+    self.head = None
+    # the last Node in the LinkedList 
+    self.tail = None 
+
+    def add_to_tail(self, data):
+        # addss `data` to the end of the LinkedList 
+        # wrap the `data` in a Node instance 
+        new_node = Node(data)
+
+
+        # call set_next with the new_node on the current tail node 
+        self.tail.set_next(new_node)
+        # update self.tail to point to the new last Node in the LinkedList 
+        self.tail = new_node 
+
+
+
+
 node = Node(1)
 node.set_next(Node(2))
+node.get_next().set_next(Node(3))
+node.get_next().get_next().set_next(Node(4))
+# YUCK
+
